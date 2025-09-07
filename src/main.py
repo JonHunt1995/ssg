430dba235dc564216041bd0a9fb85b29a0c4e554
JonHunt1995 <116927012+JonHunt1995@users.noreply.github.com>
2025-09-07 12:13:09 -0500

    Tested textnode functionality on main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,9 @@
-print('hello world')
+from textnode import TextNode, TextType
+
+def main():
+	node = TextNode("this is some anchor text", TextType.LINK, "https://www.boot.dev)")
+	print(node)
+
+# python
+if __name__ == "__main__":
+    main()
